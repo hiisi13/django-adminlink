e8fd058385bcccc3dd04187f746329a5d04b8312
Dmitry Kozhedubov <dmitry-kozhedubov@yandex.ru>
2015-03-12 21:23:51 -0700

    trying to fix setup

--- setup.py
+++ setup.py
@@ -1,22 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+
+import os
+import sys
+
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
 setup(
-    name="django-adminlink",
-    version="0.1.0",
-    license='MIT',
-    author="Dmitry Kozhedubov",
-    author_email="hiisi13@gmail.com",
+    name='django-adminlink',
+    version='0.1.0',
+    description="""Simple template tag that allows you to create links to your model's change form directly from template""",
+    author='Dmitry Kozhedubov',
+    author_email='dmitry-kozhedubov@yandex.ru',
+    url='https://github.com/hiisi13/django-adminlink',
     packages=[
-        "adminlink",
+        'adminlink',
     ],
+    include_package_data=True,
     install_requires=[
-        "Django==1.7.4",
+        'Django==1.7.4',
+    ],
+    license="MIT",
+    zip_safe=False,
+    keywords='django-management-audit',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
     ],
 )
